9c30fa8e1a042e5646a022e399bc19c37542e451
Aditya Raj Bhagat <adityabhagat900@gmail.com>
2022-12-21 07:37:12 +0545

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+
 
 app=FastAPI()
 
 @app.get("/")
 async def root():
-    return{"message":"Hello World"}
+    return{"message" : "welcome to API !!! "}
+
+@app.get("/posts")
+async def get_post():
+    return{"data" : "this is your data"}
+
+@app.post("/createposts")
+async def  create_posts(payload :dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title {payload['title']} ,content {payload['title']}"}
+    

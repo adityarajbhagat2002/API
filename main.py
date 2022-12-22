41b2be3af97017bf3fd07f1b7d75679830f72ee5
Aditya Raj Bhagat <adityabhagat900@gmail.com>
2022-12-23 01:15:06 +0545

    commit

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
-from fastapi.params import Body
+from fastapi.params import Body # it helps to import the body details from the http request we defined in the postman tool 
+from typing import Optional
+from pydantic import BaseModel # it helps us to create a schema such that what kind of data should the client(frontend) send to the backend
+
 
 
 app=FastAPI()
+
+class Post(BaseModel):
+    title : str
+    content: str
+    published : bool=True
+    rating :Optional[int] =None
 
 @app.get("/")
 async def root():
@@ -13,7 +22,11 @@ async def get_post():
     return{"data" : "this is your data"}
 
 @app.post("/createposts")
-async def  create_posts(payload :dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title {payload['title']} ,content {payload['title']}"}
+async def  create_posts(post : Post): # we are referncing the Post function class and storing it in a post variable
     
+    print(post)
+    print(post.dict())
+
+    return {"data" : "New Post created"}
+
+

559afeb379df48282fd7911f49170b41176ed135
Aditya Raj Bhagat <adityabhagat900@gmail.com>
2023-03-22 14:00:33 +0530

    commit

--- app/modules.py
+++ app/modules.py
@@ -3,4 +3,8 @@ from sqlalchemy import *
 class Post(Base):
     __tablemane__="posts"
 
-    id=Column(Integer , )
+    id=Column(Integer , primary_key= True , nullable=False)
+    title=Column(String , nullable=False)
+    content=Column(String , nullable=False)
+    published=Column(Boolean , default=True)
+    
